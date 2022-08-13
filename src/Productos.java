a327b1de8c71fb637c6e142ce6c449712c1b6733
CamiloPuche <camilopuche73@gmail.com>
2022-08-13 09:59:47 -0500

    Version Inicial

---
+++ src/Productos.java
@@ -0,0 +1,21 @@
+public class Productos {
+    String productos;
+    int Precio;
+
+    public void setProductos(String productos) {
+        this.productos = productos;
+    }
+
+    public int getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(int precio) {
+        Precio = precio;
+    }
+
+    public Productos(String productos, int Precio) {
+        this.productos = productos;
+        this.Precio = Precio;
+    }
+}
