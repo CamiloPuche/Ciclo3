ea8f1bcbfe16853fe1620a37e2751c2d13b4d354
CamiloPuche <camilopuche73@gmail.com>
2022-08-15 11:39:24 -0500

    version 3.0
    Hola mundo

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Este es un  nuevo mensaje creado");
+        System.out.println("Nuevo mensaje creado");
     }
 }
